bef45bd8fbbdb9ac9dca9b6d920b30051a1905f1
Fellipe / Desenvolvimento Official TI <desenv01@officialti.com.br>
2023-03-08 15:48:54 -0300

    lista de perguntas melhorada

--- lib/Widgets/questionario.dart
+++ lib/Widgets/questionario.dart
@@ -20,13 +20,13 @@ class Questionario extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    List<String> respostas = temPerguntaSelecionada
-        ? perguntas[perguntaSelecionada]['resposta'] as List<String>
+    List<Map<String, Object>> respostas = temPerguntaSelecionada
+        ? perguntas[perguntaSelecionada]['resposta'] as List<Map<String, Object>>
         : [];
     return Column(
       children: [
         Questao(perguntas[perguntaSelecionada]['texto'] as String),
-        ...respostas.map((t) => Resposta(t, responder)).toList(),
+        ...respostas.map((resp) => Resposta(resp['texto'] as String, responder)).toList(),
       ],
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -10,37 +10,40 @@ class _MyAppState extends State<MyApp> {
   var _perguntaSelecionada = 0;
   final _perguntas = const [
     {
-        'texto': 'Quais desses carros voce teria?',
-        'resposta': [
-          'Cayenne',
-          'M3',
-          'R8',
-          'Urus',
-        ],
-      },
-      {
-        'texto': 'Qual a cavalaria de uma lamborghini Urus 2023?',
-        'resposta': [
-          '666cv',
-          '498cv',
-          '573cv',
-          '657cv',
-        ],
-      },
-      {
-        'texto': 'Qual carro melhor?',
-        'resposta': [
-          'Urus',
-          'McDonalds',
-          'Cervejinha',
-          'Dodge RAM',
-        ],
-      },
-        
-    ];
+      'texto': 'Qual carro tem mais cavalaria (stock)?',
+      'resposta': [
+        {'texto': 'CHEVROLET Camaro', 'pontos': 1},
+        {'texto': 'PORSCHE Cayenne', 'pontos': 2},
+        {'texto': 'BMW M3', 'pontos': 3},
+        {'texto': 'AUDI R8', 'pontos': 4},
+        {'texto': 'LAMBORGHINI Urus', 'pontos': 5},
+      ],
+    },
+    {
+      'texto': 'Qual a cavalaria de uma lamborghini Urus 2023?',
+      'resposta': [
+        {'texto': '340cv', 'pontos': 1},
+        {'texto': '650cv ', 'pontos': 5},
+        {'texto': '560cv', 'pontos': 4},
+        {'texto': '780cv', 'pontos': 3},
+        {'texto': '450cv', 'pontos': 2},
+      ],
+    },
+    {
+      'texto':
+          'Qual o 0 a 100km/h de um Mercedez AMG One com cerca de 1.069cv?',
+      'resposta': [
+        {'texto': '3,6s', 'pontos': 1},
+        {'texto': '2,3s', 'pontos': 2},
+        {'texto': '3,2s', 'pontos': 3},
+        {'texto': '3,0s', 'pontos': 4},
+        {'texto': '2,9s', 'pontos': 5},
+      ],
+    },
+  ];
 
-  void _responder(){
-    if(temPerguntaSelecionada){
+  void _responder() {
+    if (temPerguntaSelecionada) {
       setState(() {
         _perguntaSelecionada++;
       });
@@ -51,8 +54,6 @@ class _MyAppState extends State<MyApp> {
     return _perguntaSelecionada < _perguntas.length;
   }
 
-
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -60,21 +61,23 @@ class _MyAppState extends State<MyApp> {
         appBar: AppBar(
           title: const Text("Perguntados"),
         ),
-        body: temPerguntaSelecionada 
-          ? Questionario(perguntaSelecionada: _perguntaSelecionada, perguntas: _perguntas, responder: _responder)
+        body: temPerguntaSelecionada
+            ? Questionario(
+                perguntaSelecionada: _perguntaSelecionada,
+                perguntas: _perguntas,
+                responder: _responder)
             : Resultado(),
       ),
     );
-  } 
+  }
 }
 
-  class MyApp extends StatefulWidget {
-    const MyApp({super.key});
-  
-    @override
-    // ignore: library_private_types_in_public_api
-    _MyAppState createState(){
-      return _MyAppState();
-    }
-    
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  // ignore: library_private_types_in_public_api
+  _MyAppState createState() {
+    return _MyAppState();
   }
+}
